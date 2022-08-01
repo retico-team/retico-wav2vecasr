8895a75ab80a594030a689c120c456774e2b3863
Casey Kennington <bakuzen@gmail.com>
2022-08-01 10:56:30 -0600

    Huggingface/Transformer implementation of wav2vec ASR

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+Setup script.
+
+Use this script to install the Huggingface wav2vec ASR incremental modules for the retico framework.
+Usage:
+    $ python3 setup.py install
+The run the simulation:
+    $ retico [-h]
+"""
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    "description": "The Huggingface wav2vec ASR incremental modules for the retico framework",
+    "author": "Tahsin Mir Imtiaz, Ryan Whetten, Casey Kennington",
+    "url": "??",
+    "download_url": "??",
+    "author_email": "caseykennington@boisestate.edu",
+    "version": "0.1",
+    "install_requires": ["retico-core~=0.2.0", "torch~=1.11.0", "hugginfave~=0.6.0", "transformers" ],
+    "packages": find_packages(),
+    "name": "retico-wav2vecasr",
+}
+
+setup(**config)
