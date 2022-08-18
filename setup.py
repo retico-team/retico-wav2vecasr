7a4118c25b3587c69de0417b37ba1ca293faed09
Thilo Michael <thilo.michael@tu-berlin.de>
2022-08-18 16:16:36 +0200

    Updated the setup for inclusion in pypi

--- setup.py
+++ setup.py
@@ -15,13 +15,22 @@ except ImportError:
 
 exec(open("retico_wav2vecasr/version.py").read())
 
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+
 config = {
     "description": "The Huggingface wav2vec ASR incremental modules for the retico framework",
     "author": "Tahsin Mir Imtiaz, Ryan Whetten, Casey Kennington, Thilo Michael",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
     "url": "https://github.com/retico-team/retico-wav2vecasr",
     "download_url": "https://github.com/retico-team/retico-wav2vecasr",
     "author_email": "caseykennington@boisestate.edu",
-    "version": "0.1",
+    "version": __version__,
     "install_requires": [
         "retico-core~=0.2",
         "transformers",
@@ -31,6 +40,12 @@ config = {
     ],
     "packages": find_packages(),
     "name": "retico-wav2vecasr",
+    "keywords": "retico, framework, incremental, dialogue, dialog, asr, speech",
+    "classifiers": [
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
 }
 
 setup(**config)
